b8cb5ee010090df0cb0896c948edbff601520151
Aiden <aiden@amxd.de>
2023-07-23 17:40:13 +0200

    Init

---
+++ src/de/amxd/knockout/main/Main.java
@@ -0,0 +1,16 @@
+package de.amxd.knockout.main;
+
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+    @Override
+    public void onDisable() {
+
+    }
+}
